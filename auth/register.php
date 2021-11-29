0f905ac8f750bcc5718e262841e101c283b36c75
Tide <tide26725@gmail.com>
2021-11-29 16:33:48 +0700

    register

---
+++ auth/register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body class="font-mali vh-100 d-flex justify-content-center align-item-center">
+    <div class="card mb-3">
+        <div class="card-header bg-primary text-white">
+            <h4>สมัครใช้งาน</h4>
+        </div>
+        <div class="card-body">
+            <form action="saveRegister.php" class="mb-3" method="post">
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="name">Email</label>
+                    <input type="email" name="email" id="email" class="form-control">
+                </div>
+            </form>
+            <a href="login.php">เข้าสู่ระบบ</a>
+        </div>
+    </div>
+    
+</body>
+</html>
