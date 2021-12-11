74029f8297ad6533dd1b37c13277a134c8cc08f8
Tide <tide26725@gmail.com>
2021-12-12 01:03:37 +0700

    box font icon

---
+++ 03_box.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Box Model</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Mali:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+  <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
+  <link rel="stylesheet" href="css/box.css">
+</head>
+
+<body>
+  <div id="b1" class="box bg-blue"><div class="big-font"><i class='bx bx-user'></i></div>
+    Box1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, dolorum? Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum porro perspiciatis libero, debitis eligendi ipsam est voluptatum reprehenderit nesciunt </div>
+  <div id="b2" class="box bg-green"><div class="big-font"><i class='bx bxs-lemon bx-flashing' ></i></div>
+    Box2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsam inventore molestias quisquam voluptatem tenetur ullam voluptatibus atque. Consequuntur, nobis!</div>
+</body>
+
+</html>
