4642fbf672c991ea9c8e4f761f545d8085b59ece
Tide <tide26725@gmail.com>
2021-12-12 17:39:15 +0700

    position_1

---
+++ 04_position.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Position</title>
+  <link rel="stylesheet" href="css/position.css">
+</head>
+<body>
+  <div id="b1" class="box">Box1 Static</div>
+  <div id="b2" class="box">Box2 Absolute</div>
+  <div id="b3" class="box">
+    Box3 Relative
+    <div id="b4" class="box_mini">Box4 Abmini</div>
+  </div>
+</body>
+</html>
