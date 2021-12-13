380cbd2f3fd4b4e303428fbb6a0030422489e7ad
Tide <tide26725@gmail.com>
2021-12-13 13:53:14 +0700

    flex

---
+++ 05_flex.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Flex</title>
+  <link rel="stylesheet" href="css/flex.css">
+</head>
+<body>
+  <div class="menu-container">
+    <a href="03_box.php">Box</a>
+    <a href="04_position.php">Position</a>
+    <a href="10_pdo.php">PDO</a>
+    <a href="#">Exit</a>
+  </div>
+  <!-- <div class="container">
+    <div class="item">Item1</div>
+    <div class="item">Item2</div>
+    <div class="item" id="i3">Item3</div>
+    <div class="item">Item4</div> 
+    <div class="item">Item5</div>
+    <div class="item">Item6</div>
+    <div class="item">Item7</div>
+    <div class="item">Item8</div>
+    <div class="item">Item9</div>
+  </div> -->
+  
+  <div class="login-container">
+    <div class="login-box">
+      <form action="">
+        <h2>Login</h2>
+        <div class="form-row">
+          <label for="user">User</label>
+          <input type="text" name="user">
+        </div>
+        <div class="form-row">
+          <label for="password">Password</label>
+          <input type="password" name="password">
+        </div>
+        <div class="form-row">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  </div>
+
+  <div class="news-container">
+    <div class="news-column">
+      <img src="https://images.unsplash.com/photo-1568605117036-5fe5e7bab0b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque eligendi accusantium, itaque voluptates, iure perspiciatis dolore similique harum nisi atque ut dolorem consequatur libero alias ducimus natus assumenda aperiam veritatis? Omnis quae corrupti nulla similique aspernatur, at tempora? In laborum explicabo illo autem quo consequatur vitae repellat facilis consectetur, eveniet id distinctio nulla iure quisquam reprehenderit officiis! Aliquam eos, hic nisi culpa repellat vel in numquam eum. Est, ex. Voluptate magnam corporis veniam velit alias! Velit iusto similique aut ducimus deleniti, earum dicta facere distinctio obcaecati tenetur? Assumenda doloribus mollitia obcaecati voluptatem molestias fugiat error? Aperiam consequuntur veniam architecto sint.</p>
+    </div>
+    <div class="news-column" id="news2">
+      <img src="https://images.unsplash.com/photo-1493238792000-8113da705763?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque eligendi accusantium, itaque voluptates, iure perspiciatis dolore similique harum nisi atque ut dolorem consequatur libero alias ducimus natus assumenda aperiam veritatis? Omnis quae corrupti nulla similique aspernatur, at tempora? In laborum explicabo illo autem quo consequatur vitae repellat facilis consectetur, eveniet id distinctio nulla iure quisquam reprehenderit officiis! Aliquam eos, hic nisi culpa repellat vel in numquam eum. Est, ex. Voluptate magnam corporis veniam velit alias! Velit iusto similique aut ducimus deleniti, earum dicta facere distinctio obcaecati tenetur? Assumenda doloribus mollitia obcaecati voluptatem molestias fugiat error? Aperiam consequuntur veniam architecto sint.</p>
+    </div>
+    <div class="news-column">
+      <img src="https://images.unsplash.com/photo-1502877338535-766e1452684a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80" alt="">
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque eligendi accusantium, itaque voluptates, iure perspiciatis dolore similique harum nisi atque ut dolorem consequatur libero alias ducimus natus assumenda aperiam veritatis? Omnis quae corrupti nulla similique aspernatur, at tempora? In laborum explicabo illo autem quo consequatur vitae repellat facilis consectetur, eveniet id distinctio nulla iure quisquam reprehenderit officiis! Aliquam eos, hic nisi culpa repellat vel in numquam eum. Est, ex. Voluptate magnam corporis veniam velit alias! Velit iusto similique aut ducimus deleniti, earum dicta facere distinctio obcaecati tenetur? Assumenda doloribus mollitia obcaecati voluptatem molestias fugiat error? Aperiam consequuntur veniam architecto sint.</p>
+    </div>
+  </div>
+</body>
+</html>
